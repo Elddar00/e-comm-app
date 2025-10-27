690a11f786f927215a26f5ffcc5a0e04eab2b259
Elddar00 <eldaridrizovic00@gmail.com>
2025-10-27 16:21:30 +0100

    Fastify Microservice Authentication

--- apps/order-service/src/index.ts
+++ apps/order-service/src/index.ts
@@ -1,6 +1,9 @@
 import Fastify from "fastify";
+import { clerkPlugin, getAuth } from "@clerk/fastify";
 
 const fastify = Fastify();
+
+fastify.register(clerkPlugin);
 
 fastify.get("/health", (request, replay) => {
   return replay.status(200).send({
@@ -8,6 +11,14 @@ fastify.get("/health", (request, replay) => {
     uptime: process.uptime(),
     timestamp: Date.now(),
   });
+});
+
+fastify.get("/test", (request, replay) => {
+  const { userId } = getAuth(request);
+  if (!userId) {
+    return replay.send({ message: "You are not logged in" });
+  }
+  return replay.send({ message: "Order service is authenticated" });
 });
 
 const start = async () => {
