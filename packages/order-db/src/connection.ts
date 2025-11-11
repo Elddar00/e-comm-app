169d64dee4ed1ccf3d1b759f389ee4db69b80c88
Elddar00 <eldaridrizovic00@gmail.com>
2025-11-11 16:14:24 +0100

    Creating Database for Order Service - MongoDB

---
+++ packages/order-db/src/connection.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectOrderDB = async () => {
+  if (isConnected) return;
+
+  if (!process.env.MONGO_URL) {
+    throw new Error("MONGO_URL is not defined in env file!");
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    isConnected = true;
+  } catch (error) {
+    console.log(error);
+  }
+};
