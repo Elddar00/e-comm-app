ac985e8c906e99371a5eadfef7aedaf12863c5a9
Elddar00 <eldaridrizovic00@gmail.com>
2025-11-13 23:18:16 +0100

    Role Based Auth & Turborepo Share Types

--- apps/order-service/src/middleware/authMiddleware.ts
+++ apps/order-service/src/middleware/authMiddleware.ts
@@ -1,5 +1,6 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { clerkPlugin, getAuth } from "@clerk/fastify";
+import type { CustomJwtSessionClaims } from "@repo/types";
 
 declare module "fastify" {
   interface FastifyRequest {
@@ -13,8 +14,26 @@ export const shouldBeUser = async (
 ) => {
   const { userId } = getAuth(request);
   if (!userId) {
-    return replay.send({ message: "You are not logged in" });
+    return replay.status(401).send({ message: "You are not logged in" });
   }
 
   request.userId = userId;
+};
+
+export const shouldBeAdmin = async (
+  request: FastifyRequest,
+  replay: FastifyReply
+) => {
+  const auth = getAuth(request);
+  if (!auth.userId) {
+    return replay.status(401).send({ message: "You are not logged in" });
+  }
+
+  const claims = auth.sessionClaims as CustomJwtSessionClaims;
+
+  if (claims.metadata?.role !== "admin") {
+    return replay.status(403).send({ message: "Unauthorized!" });
+  }
+
+  request.userId = auth.userId;
 };
