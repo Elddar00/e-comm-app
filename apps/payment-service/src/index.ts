dcbd889660adcec1ce4a84cfbb1bac08b1107f30
Elddar00 <eldaridrizovic00@gmail.com>
2025-11-21 17:39:56 +0100

    E-commerce Application Accepting Payments

--- apps/payment-service/src/index.ts
+++ apps/payment-service/src/index.ts
@@ -2,9 +2,12 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { shouldBeUser } from "./middleware/authMiddleware.js";
+import stripe from "./utils/stripe.js";
+import { cors } from "hono/cors";
 
 const app = new Hono();
 app.use("*", clerkMiddleware());
+app.use("*", cors({ origin: ["http://localhost:3001"] }));
 
 app.get("/health", (c) => {
   return c.json({
@@ -14,24 +17,34 @@ app.get("/health", (c) => {
   });
 });
 
-app.get("/test", shouldBeUser, (c) => {
-  return c.json({
-    message: "Payment service is Authenticated",
-    userId: c.get("userId"),
-  });
-});
+// app.post("/create-stripe-product", async (c) => {
+//   const res = await stripe.products.create({
+//     id: "123",
+//     name: "Test Product",
+//     default_price_data: {
+//       currency: "usd",
+//       unit_amount: 10 * 100,
+//     },
+//   });
+
+//   return c.json(res);
+// });
+
+// app.get("/stripe-product-price", async (c) => {
+//   const res = await stripe.prices.list({
+//     product: "123",
+//   });
+
+//   return c.json(res);
+// });
 
 const start = async () => {
   try {
-    serve(
-      {
-        fetch: app.fetch,
-        port: 8002,
-      },
-      (info) => {
-        console.log(`Payment service is running on port 8002`);
-      }
-    );
+    serve({
+      fetch: app.fetch,
+      port: 8002,
+    });
+    console.log("Payment service is running on port 8002");
   } catch (error) {
     console.log(error);
     process.exit(1);
